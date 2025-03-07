3fe7174dcb54d34356483b28a743230827da97d1
vgdhieu0602 <vg.dhieu0602@gmail.com>
2025-03-07 15:09:39 +0700

    finish setup theming and font

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = async () => {
     <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
       <nav className="flex justify-between items-center">
         <Link href="/">
-          <Image src="/logo.jpg" alt="logo" width={144} height={30} />
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
 
         <div className="flex items-center gap-5 text-black">
